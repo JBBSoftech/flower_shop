275e0235a811f62b9da1aae546ea25281e682079
JBBSoftech <jbbsoftech@gmail.com>
2025-12-03 14:12:26 +0530

    Update Appifyours/frontend/lib/main.dart

--- Appifyours/frontend/lib/main.dart
+++ Appifyours/frontend/lib/main.dart
@@ -8,7 +8,7 @@ import 'dart:async';
 // Dynamic Product Data
 final List<Map<String, dynamic>> productCards = [
   {
-      'id': '5tby3qeeu',
+      'id': 'tl7ow1rs0',
       'productName': 'Rose',
       'price': '250',
       'discountPrice': '20',
@@ -16,7 +16,7 @@ final List<Map<String, dynamic>> productCards = [
       'category': 'Piece',
     },
   {
-      'id': 'ajdhnrfmn',
+      'id': 'edaoqzozd',
       'productName': 'Sun Flower',
       'price': '150',
       'discountPrice': '30',
@@ -24,7 +24,7 @@ final List<Map<String, dynamic>> productCards = [
       'category': 'Piece',
     },
   {
-      'id': '2zmdltzer',
+      'id': 'z5u1rmxl9',
       'productName': 'Tea',
       'price': '50',
       'discountPrice': '10',
